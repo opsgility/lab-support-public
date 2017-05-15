9e38ee8cfa3bd57ad925fff700316c7a961450cf
Dan Patrick <dan@opsgility.com>
2017-05-15 18:43:31 -0400

    change to CLI install command

--- script-extensions/linuxlabvmconfig.sh
+++ script-extensions/linuxlabvmconfig.sh
@@ -8,7 +8,7 @@ apt-get install xrdp -y
 apt-get update
 apt-get install -y docker.io
 #install Azure CLI 2.0 to run locally
-curl -L https://aka.ms/InstallAzureCli | bash
+apt-get install azure-cli -y
 #installing visual studio code which can be launched from accessories
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg

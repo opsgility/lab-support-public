d1a5250408cd74c7ea86d3a430b4dae34e6b2666
John Adams <john@opsgility.com>
2017-06-10 13:42:25 -0500

    update docker install to docker-ce, add java script

--- script-extensions/linuxlabvmconfigwithjava.sh
+++ script-extensions/linuxlabvmconfigwithjava.sh
@@ -34,3 +34,15 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
 apt-get install -y docker-ce
+
+#download and install the Java JDK
+add-apt-repository ppa:webupd8team/java
+apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | \
+    /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
+
+#download and install maven
+apt-get update
+apt-get install -y maven
--- script-extensions/linuxlabvmconfigwithps.sh
+++ script-extensions/linuxlabvmconfigwithps.sh
@@ -5,10 +5,19 @@ apt-get install lxde -y
 apt-get install xrdp -y
 echo startlxde > ~/.xsession
 /etc/init.d/xrdp start
-#install docker which will then allow for running the az cli from an container
-#to run the cli container open terminal and use sudo docker run -it azuresdk/azure-cli-python:latest
+
+#install docker
+#to run the az cli container open terminal and use 'sudo docker run -it azuresdk/azure-cli-python:latest'
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker-ce
+
 #installing visual studio code which can be launched from accessories
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
@@ -16,11 +25,13 @@ sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable
 sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
 apt-get update
 apt-get install code
+
 #Install DotNet Core for linux
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.3
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+apt-get update
+apt-get install dotnet-dev-1.0.4
+
 #installing powershell for linux
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list

2fa476c05b17f73443b15cfb741debe43d174668
John Adams <john@opsgility.com>
2017-06-20 15:27:18 -0500

    add service fabric VM script

---
+++ script-extensions/linuxlabvmconfigwithservicefabric.sh
@@ -0,0 +1,70 @@
+#Install LXDE lxde.org and xrdp - (make sure to open 3389 on the NSG of the azure vm)
+apt-get update
+apt-get install lxde -y
+apt-get install xrdp -y
+/etc/init.d/xrdp start
+
+#installing visual studio code which can be launched from accessories
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
+apt-get update
+apt-get install code
+
+#Prepare XWindows System
+wget https://opsgilityweb.blob.core.windows.net/test/xsession
+mv xsession /home/demouser/.xsession
+
+#install the Azure CLI using instructions from Azure.com
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+apt-get install apt-transport-https -y
+apt-get update && sudo apt-get install azure-cli -y
+
+#install docker
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+apt-get update
+apt-get install -y docker-ce
+
+#download and install the Java JDK
+add-apt-repository ppa:webupd8team/java
+apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | \
+    /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer
+apt-get install -y oracle-java8-set-default
+
+#download and install maven
+apt-get update
+apt-get install -y maven
+
+#download, extract, and create shorcut for Azure Storage Explorer
+mkdir /usr/share/storageexplorer
+wget https://go.microsoft.com/fwlink/?LinkId=722418 -O /usr/share/storageexplorer/StorageExplorer.tar.gz
+tar -xvzf /usr/share/storageexplorer/StorageExplorer.tar.gz -C /usr/share/storageexplorer
+wget https://raw.githubusercontent.com/opsgility/lab-support-public/master/script-extensions/storageexplorer.desktop -O /usr/share/applications/storageexplorer.desktop
+chmod a+x /usr/share/applications/storageexplorer.desktop
+
+#download and install Azure Service Fabric and DotNet Core tools
+sh -c 'echo "deb [arch=amd64] http://apt-mo.trafficmanager.net/repos/servicefabric/ trusty main" > /etc/apt/sources.list.d/servicefabric.list'
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+apt-get update
+apt-get install servicefabricsdkcommon
+echo "servicefabric servicefabric/accepted-eula-v1 select true" | debconf-set-selections
+echo "servicefabricsdkcommon servicefabricsdkcommon/accepted-eula-v1 select true" | debconf-set-selections
+/opt/microsoft/sdk/servicefabric/common/sdkcommonsetup.sh
+/opt/microsoft/sdk/servicefabric/common/clustersetup/devclustersetup.sh
+apt-get install servicefabricsdkjava
+/opt/microsoft/sdk/servicefabric/java/sdkjavasetup.sh
+apt-get install servicefabricsdkcsharp
+/opt/microsoft/sdk/servicefabric/csharp/sdkcsharpsetup.sh

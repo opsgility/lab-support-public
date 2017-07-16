20ad50adcfeea4b49e06901e55846740a9ebfc6e
Michael Washam <michael@opsgility.com>
2017-07-16 15:41:36 -0500

    more updates

--- script-extensions/linuxlabvmconfig-vnc.sh
+++ script-extensions/linuxlabvmconfig-vnc.sh
@@ -49,21 +49,21 @@ tar -xvzf /usr/share/storageexplorer/StorageExplorer.tar.gz -C /usr/share/storag
 wget https://raw.githubusercontent.com/opsgility/lab-support-public/master/script-extensions/storageexplorer.desktop -O /usr/share/applications/storageexplorer.desktop
 chmod a+x /usr/share/applications/storageexplorer.desktop
 
-#apt-get update 
-#apt-get install expect -y
+apt-get update 
+apt-get install expect -y
 
 # Setup VNC 
-/usr/bin/expect <<EOF
-spawn su demouser -c "sudo /usr/bin/vncserver"
-expect "Password:"
-send "$1\r"
-expect "Verify:"
-send "$1\r"
-expect "(y/n?"
-send "n\r"
-expect eof
-EOF
-vncserver -kill :1
+#/usr/bin/expect <<EOF
+#spawn su demouser -c "sudo /usr/bin/vncserver"
+#expect "Password:"
+#send "$1\r"
+#expect "Verify:"
+#send "$1\r"
+#expect "(y/n?"
+#send "n\r"
+#expect eof
+#EOF
+#vncserver -kill :1
 
 # Enable copy & paste
 apt-get update 
@@ -74,10 +74,20 @@ autocutsel -fork
 #mkdir /home/demouser/.vnc
 
 # Setup VNC start environment 
-wget https://raw.githubusercontent.com/opsgility/lab-support-public/master/script-extensions/xstartup
-mv xstartup /home/demouser/.vnc/xstartup
-chmod 0755 /home/demouser/.vnc/xstartup
-
-echo "vncserver" >> /home/demouser/.profile
+#wget https://raw.githubusercontent.com/opsgility/lab-support-public/master/script-extensions/xstartup
+#mv xstartup /home/demouser/.vnc/xstartup
+#chmod 0755 /home/demouser/.vnc/xstartup
 
 
+# Setup VNC in the user's profile 
+echo "/usr/bin/expect <<EOF" >> /home/demouser/.profile
+echo "spawn su demouser -c \"sudo /usr/bin/vncserver\"" >> /home/demouser/.profile
+echo "expect \"Password:\"" >> /home/demouser/.profile
+echo "send \"$1\r\"" >> /home/demouser/.profile
+echo "expect \"Verify:\"" >> /home/demouser/.profile
+echo "send \"$1\r\"" >> /home/demouser/.profile
+echo "expect \"(y/n?\"" >> /home/demouser/.profile
+echo "send \"n\r\"" >> /home/demouser/.profile
+echo "expect eof" >> /home/demouser/.profile
+echo "EOF" >> /home/demouser/.profile
+echo "autocutsel -fork" >> /home/demouser/.profile

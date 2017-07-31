e29e8d7ddfbc5ccc870af1af7b234f6aa157ef7b
John Adams <john@opsgility.com>
2017-07-31 14:01:19 -0500

    add install-service-fabric.ps1 script extension

---
+++ script-extensions/install-service-fabric.ps1
@@ -0,0 +1,15 @@
+Set-Location d:
+
+mkdir temp
+
+Set-Location temp
+
+$webclient = New-Object System.Net.WebClient
+
+$webclient.DownloadFile("https://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi", "d:\temp\wpi.msi")
+
+d:\temp\wpi.msi /passive
+
+Set-Location 'C:\Program Files\Microsoft\Web Platform Installer'
+
+.\WebpiCmd.exe /Install /Products:'Microsoft Azure Service Fabric SDK - 2.6.220' /AcceptEula

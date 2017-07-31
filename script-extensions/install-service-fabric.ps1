6a24a636f7a63c3374246e62e4753e4b62a17693
John Adams <john@opsgility.com>
2017-07-31 17:48:00 -0500

    update install-service-fabric.ps1 again

--- script-extensions/install-service-fabric.ps1
+++ script-extensions/install-service-fabric.ps1
@@ -8,7 +8,7 @@ $webclient = New-Object System.Net.WebClient
 
 $webclient.DownloadFile("https://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi", "d:\temp\wpi.msi")
 
-d:\temp\wpi.msi /passive
+d:\temp\wpi.msi /quiet
 
 $wpidir = "C:\Program Files\Microsoft\Web Platform Installer"
 

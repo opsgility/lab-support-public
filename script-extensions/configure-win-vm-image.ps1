1fa15cb996a87a5c18b2c73e09299d284dee52f5
Michael Washam <michael@opsgility.com>
2017-10-14 07:21:11 -0500

    updates

--- script-extensions/configure-win-vm-image.ps1
+++ script-extensions/configure-win-vm-image.ps1
@@ -1,4 +1,4 @@
-param($sourceFileUrl, $destinationFolder, $labName="authoring-arm-templates-vs-2017")
+param($sourceFileUrl="", $destinationFolder="", $labName="")
 $ErrorActionPreference = 'SilentlyContinue'
 
 if([string]::IsNullOrEmpty($sourceFileUrl) -eq $false -and [string]::IsNullOrEmpty($destinationFolder) -eq $false)

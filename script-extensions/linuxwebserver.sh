a1742e7d45434d8e77efe5ae0859641b4f30fec0
paulopsgility <paul@opsgility.com>
2017-11-10 12:04:22 -0600

    Typo

--- script-extensions/linuxwebserver.sh
+++ script-extensions/linuxwebserver.sh
@@ -1,4 +1,4 @@
 export DEBIAN_FRONTEND=noninteractive
 #Install LXDE lxde.org and xrdp - (make sure to open 3389 on the NSG of the azure vm)
-apt-get update
-apt-get install apache2
+apt-get update -y
+apt-get install apache2 -y

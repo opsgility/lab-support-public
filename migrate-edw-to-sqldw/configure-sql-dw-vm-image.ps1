af807d96fd2bf1293871fa3bfe8eafb8b0f1b810
paulopsgility <paul@opsgility.com>
2017-11-22 12:39:26 -0600

    Cust script ext for sqldw lab

---
+++ migrate-edw-to-sqldw/configure-sql-dw-vm-image.ps1
@@ -0,0 +1,107 @@
+param($sourceFileUrl="http://opsgilitylabs.blob.core.windows.net/online-labs/sql-server-iaas-leverage-azure-storage/StudentFiles.zip", $destinationFolder="C:\OpsgilityTraining", $labName="sql-server-iaas-leverage-azure-storage")
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+#put in an artificial wait to let things settle down before we start making changes
+Start-Sleep -s 240
+
+if([string]::IsNullOrEmpty($sourceFileUrl) -eq $false -and [string]::IsNullOrEmpty($destinationFolder) -eq $false)
+{
+    if((Test-Path $destinationFolder) -eq $false)
+    {
+        New-Item -Path $destinationFolder -ItemType directory
+    }
+    $splitpath = $sourceFileUrl.Split("/")
+    $fileName = $splitpath[$splitpath.Length-1]
+    $destinationPath = Join-Path $destinationFolder $fileName
+
+    (New-Object Net.WebClient).DownloadFile($sourceFileUrl,$destinationPath);
+
+    (new-object -com shell.application).namespace($destinationFolder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
+}
+
+# Disable IE Enhanced Security Configuration
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+New-Item -Path $adminKey -Force
+New-Item -Path $UserKey -Force
+New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\Software\Microsoft\Internet Explorer\Security"
+New-ItemProperty -Path $HKLM -Name "DisableSecuritySettingsCheck" -Value 1 -PropertyType DWORD
+Set-ItemProperty -Path $HKLM -Name "DisableSecuritySettingsCheck" -Value 1
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+
+
+### Extract Zip -- <<<comment this line out for uncompressed db files>>>
+Expand-Archive $destinationPath -DestinationPath $destinationFolder -Force
+
+### Create SQLDATA Directory
+New-Item -ItemType Directory -Force -Path C:\ -Name SQLDATA
+
+$spassword =  ConvertTo-SecureString "$password" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$user", $spassword)
+
+Enable-PSRemoting -Force
+Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList "Password", $spassword -ScriptBlock { 
+
+    # Setup mixed mode authentication
+    Import-Module "sqlps" -DisableNameChecking
+    [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
+    $sqlesq = new-object ('Microsoft.SqlServer.Management.Smo.Server') Localhost
+    $sqlesq.Settings.LoginMode = [Microsoft.SqlServer.Management.Smo.ServerLoginMode]::Mixed
+    $sqlesq.Alter() 
+
+    # Enable TCP Server Network Protocol
+    $smo = 'Microsoft.SqlServer.Management.Smo.'  
+    $wmi = new-object ($smo + 'Wmi.ManagedComputer').  
+    $uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']"  
+    $Tcp = $wmi.GetSmoObject($uri)  
+    $Tcp.IsEnabled = $true  
+    $Tcp.Alter() 
+
+    # Open firewall for SQL 
+    New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound -Protocol TCP -LocalPort 1433 -Action allow 
+
+    # Add local administrators group as sysadmin
+    Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "CREATE LOGIN [BUILTIN\Administrators] FROM WINDOWS"
+    Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "ALTER SERVER ROLE sysadmin ADD MEMBER [BUILTIN\Administrators]"
+
+    # Restore the database from the backup
+    Invoke-Sqlcmd -ServerInstance Localhost -Database "master" -Query "RESTORE DATABASE CohoDW FROM DISK = 'C:\LabFiles\AdventureWorksDW2016CTP3.bak' WITH MOVE 'CohoDW_Data' TO 'C:\SQLDATA\CohoDW_Data.mdf', MOVE 'CohoDW_Log' TO 'C:\SQLDATA\CohoDW_Log.ldf'"
+
+}
+Disable-PSRemoting -Force
+
+#Open firewall
+New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound �Protocol TCP �LocalPort 1433 -Action allow 
+
+# Enable TCP Server Network Protocol
+$smo = 'Microsoft.SqlServer.Management.Smo.'  
+$wmi = new-object ($smo + 'Wmi.ManagedComputer').  
+$uri = "ManagedComputer[@Name='" + (get-item env:\computername).Value + "']/ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']"  
+$Tcp = $wmi.GetSmoObject($uri)  
+$Tcp.IsEnabled = $true  
+$Tcp.Alter() 
+
+# Restart the SQL Server service
+Restart-Service -Name "MSSQLSERVER" -Force

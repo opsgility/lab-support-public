8a2781df986843f62cef3cab519df85b7cfcdc0a
Dan Patrick <dan@opsgility.com>
2017-11-30 12:45:12 -0500

    continued updates to linux lift and shift development

---
+++ linux-lift-shift/scaleset/osticketscalesets.sh
@@ -0,0 +1,17 @@
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password demo@pass123'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password demo@pass123'
+sudo apt-get install -y lamp-server^
+MYSQL=`which mysql`
+Q1="CREATE DATABASE IF NOT EXISTS osticket;"
+Q2="GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+Q3="CREATE USER 'osticket'@'%' IDENTIFIED BY 'demo@pass123';"
+Q4="GRANT ALL PRIVILEGES ON *.* TO 'osticket'@'%' WITH GRANT OPTION;"
+SQL="${Q1}${Q2}${Q3}${Q4}"
+$MYSQL -u root --password=demo@pass123 -e "$SQL"
+sudo apt-get install unzip -y
+sudo apt-get update -y
+sudo wget https://github.com/opsgility/lab-support-public/raw/master/linux-lift-shift/scaleset/osticket.zip
+sudo unzip osticket.zip -d /var/www/html
+sudo chown -R demouser:www-data /var/www/html
+mv /var/www/html/index.html /var/www/html/index.html.org
+sudo systemctl restart apache2

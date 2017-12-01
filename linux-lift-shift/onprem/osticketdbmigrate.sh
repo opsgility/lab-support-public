2b68d1ad5f7f98e84cde7075d7bdcffadb0bf7a9
Dan Patrick <dan@opsgility.com>
2017-11-30 19:30:35 -0500

    adding a apt-get update on top on all scripts.

--- linux-lift-shift/onprem/osticketdbmigrate.sh
+++ linux-lift-shift/onprem/osticketdbmigrate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+adding a apt-get update on top
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password demo@pass123'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password demo@pass123'
 sudo apt-get install -y lamp-server^
@@ -16,5 +17,5 @@ sudo apt-get update -y
 sudo wget https://github.com/opsgility/lab-support-public/raw/master/linux-lift-shift/onprem/osticket.zip
 sudo unzip osticket.zip -d /var/www/html
 sudo chown -R demouser:www-data /var/www/html
-mv /var/www/html/index.html /var/www/html/index.html.org
+sudo mv /var/www/html/index.html /var/www/html/index.html.org
 sudo systemctl restart apache2
--- linux-lift-shift/scaleset/osticketscalesets.sh
+++ linux-lift-shift/scaleset/osticketscalesets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update -y
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password demo@pass123'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password demo@pass123'
 sudo apt-get install -y lamp-server^
@@ -9,3 +10,4 @@ sudo unzip osticket.zip -d /var/www/html
 sudo chown -R demouser:www-data /var/www/html
 sudo mv /var/www/html/index.html /var/www/html/index.html.org
 sudo systemctl restart apache2
+

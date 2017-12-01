c49223fa6cca53b9733542bfa25f2b9f390be0e6
Dan Patrick <dan@opsgility.com>
2017-11-30 19:52:04 -0500

    looking for bugs

--- linux-lift-shift/scaleset/osticketscalesets.sh
+++ linux-lift-shift/scaleset/osticketscalesets.sh
@@ -7,7 +7,7 @@ sudo apt-get install unzip -y
 sudo apt-get update -y
 sudo wget https://github.com/opsgility/lab-support-public/raw/master/linux-lift-shift/scaleset/osticket.zip
 sudo unzip osticket.zip -d /var/www/html
-sudo chown -R demouser:www-data /var/www/html
+#sudo chown -R demouser:www-data /var/www/html
 sudo mv /var/www/html/index.html /var/www/html/index.html.org
 sudo systemctl restart apache2
 

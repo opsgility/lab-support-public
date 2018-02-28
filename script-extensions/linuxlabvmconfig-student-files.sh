9bbbe01e73aa275e41e6b5702262e6347ec933a4
Michael Washam <michael@opsgility.com>
2018-02-28 06:13:55 -0600

    updates

--- script-extensions/linuxlabvmconfig-student-files.sh
+++ script-extensions/linuxlabvmconfig-student-files.sh
@@ -1,5 +1,5 @@
-DOWNLOADURL=$1
 export DEBIAN_FRONTEND=noninteractive
+DOWNLOADURL=$1
 #Install LXDE lxde.org and xrdp - (make sure to open 3389 on the NSG of the azure vm)
 apt-get update
 apt-get install lxde -y

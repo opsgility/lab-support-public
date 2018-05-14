c23d904c10c7d9c283277cff32e13b005f3f641b
Michael Washam <michael@opsgility.com>
2018-05-14 07:26:19 -0500

    updates to support rdp fix

--- sql-server-iaas-availability-groups/configure-sqlvm1.ps1
+++ sql-server-iaas-availability-groups/configure-sqlvm1.ps1
@@ -128,4 +128,19 @@ Disable-PSRemoting -Force
 
 #Join Domain
 $domCredential = New-Object System.Management.Automation.PSCredential("$domain\$user", $spassword)
-Add-Computer -DomainName "$domain" -Credential $domCredential -Restart -Force
+#Add-Computer -DomainName "$domain" -Credential $domCredential -Restart -Force
+
+Add-Computer -DomainName "$domain" -Credential $domCredential -Force
+
+# Download RDP fix 
+$url = "https://opsgilitylabs.blob.core.windows.net/rdp-fix/windows10.0-kb4093120-x64_72c7d6ce20eb42c0df760cd13a917bbc1e57c0b7.msu"
+$output = "C:\OpsgilityTraining\windows10.0-kb4093120-x64_72c7d6ce20eb42c0df760cd13a917bbc1e57c0b7.msu"
+
+if((Test-Path -Path "C:\OpsgilityTraining") -eq $false) {
+
+    New-Item -Path "C:\OpsgilityTraining" -ItemType Directory
+}
+Invoke-WebRequest -Uri $url -OutFile $output
+
+
+& wusa.exe C:\OpsgilityTraining\windows10.0-kb4093120-x64_72c7d6ce20eb42c0df760cd13a917bbc1e57c0b7.msu /quiet /forcerestart

2887b37b0061e9576f6ab5ca945c0ecb3940258d
paulopsgility <paul@opsgility.com>
2018-05-15 18:35:08 -0400

    updates for lab perf

--- sql-server-iaas-availability-groups/configure-sqlvm2.ps1
+++ sql-server-iaas-availability-groups/configure-sqlvm2.ps1
@@ -114,16 +114,18 @@ Disable-PSRemoting -Force
 
 #Join Domain
 $domCredential = New-Object System.Management.Automation.PSCredential("$domain\$user", $spassword)
-Add-Computer -DomainName "$domain" -Credential $domCredential -Restart -Force
+#Add-Computer -DomainName "$domain" -Credential $domCredential -Restart -Force
 
-#Add-Computer -DomainName "$domain" -Credential $domCredential -Force
+Add-Computer -DomainName "$domain" -Credential $domCredential -Force
 
 # Download RDP fix 
-#$url = "https://opsgilitylabs.blob.core.windows.net/rdp-fix/windows10.0-kb4093120-x64_72c7d6ce20eb42c0df760cd13a917bbc1e57c0b7.msu"
-#$output = "C:\OpsgilityTraining\windows10.0-kb4093120-x64_72c7d6ce20eb42c0df760cd13a917bbc1e57c0b7.msu"
+$url = "https://opsgilitylabs.blob.core.windows.net/rdp-fix/windows10.0-kb4103723-x64_2adf2ea2d09b3052d241c40ba55e89741121e07e.msu"
+$output = "C:\OpsgilityTraining\windows10.0-kb4103723-x64_2adf2ea2d09b3052d241c40ba55e89741121e07e.msu"
 
-#if((Test-Path -Path "C:\OpsgilityTraining") -eq $false) {
+if((Test-Path -Path "C:\OpsgilityTraining") -eq $false) {
 
-#    New-Item -Path "C:\OpsgilityTraining" -ItemType Directory
-#}
-#Invoke-WebRequest -Uri $url -OutFile $output
+    New-Item -Path "C:\OpsgilityTraining" -ItemType Directory
+}
+Invoke-WebRequest -Uri $url -OutFile $output
+
+& wusa.exe C:\OpsgilityTraining\windows10.0-kb4103723-x64_2adf2ea2d09b3052d241c40ba55e89741121e07e.msu /quiet /forcerestart

938550044fe1a2dd5700e56c8082052f2c768f9e
Chris Pietschmann <chris@buildazure.com>
2018-05-17 11:47:14 -0500

    added CLI 2 to VS Code vm configure script

--- script-extensions/configure-win-vm-image-vscode.ps1
+++ script-extensions/configure-win-vm-image-vscode.ps1
@@ -143,5 +143,15 @@ Start-Process -FilePath $Path\$Installer -Args "/verysilent /MERGETASKS=!runcode
 Remove-Item $Path\$Installer
 
 
+# Install Azure CLI 2
+$Path = $env:TEMP; 
+$Installer = "cli_installer.msi"
+Write-Host "Downloading Azure CLI 2..." -ForegroundColor Green
+Invoke-WebRequest "https://aka.ms/InstallAzureCliWindows" -OutFile $Path\$Installer
+Write-Host "Installing Azure CLI from $Path\$Installer..." -ForegroundColor Green
+Start-Process -FilePath msiexec -Args "/i $Path\$Installer /quiet /qn /norestart" -Verb RunAs -Wait
+Remove-Item $Path\$Installer
+
+
 
 

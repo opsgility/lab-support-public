965b47ddcdb7535bf3a3fad9650b8b6ef5fc7c3f
Michael Washam <michael@opsgility.com>
2019-02-28 08:40:31 -0600

    adding apache install

--- script-extensions/install-apache.sh
+++ script-extensions/install-apache.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 #install-apache.sh
 apt-get update
-apt-get -y install apache2 php7.0 libapache2-mod-php7.0
-apt-get -y install php-mysql
-sudo a2enmod php7.0
-apachectl restart
+sudo apt install apache2

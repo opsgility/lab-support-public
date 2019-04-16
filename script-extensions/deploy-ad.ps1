75bdb6011b4a3573052e9f9cb44fb1948b07856f
Scott Hoag <scottmhoag@gmail.com>
2019-04-16 09:39:40 -0400

    Update deploy-ad.ps1
    Added explicit install of DNS and associated tooling, including Po$h module

--- script-extensions/deploy-ad.ps1
+++ script-extensions/deploy-ad.ps1
@@ -4,11 +4,12 @@ $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
 
 Install-WindowsFeature -Name "AD-Domain-Services" `
                        -IncludeManagementTools `
-                       -IncludeAllSubFeature 
+                       -IncludeAllSubFeature
 
- Install-ADDSForest -DomainName $domain `
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+
+Install-ADDSForest -DomainName $domain `
                    -DomainMode Win2012 `
                    -ForestMode Win2012 `
                    -Force `
-                   -SafeModeAdministratorPassword $smPassword 
-
+                   -SafeModeAdministratorPassword $smPassword

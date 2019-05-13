a748d73b7d5084cc4aaaa3ea75c860db4085bcfb
Scott Hoag <scottmhoag@gmail.com>
2019-05-13 11:28:37 -0400

    Update docker-build-agent.sh

--- script-extensions/docker-build-agent.sh
+++ script-extensions/docker-build-agent.sh
@@ -7,7 +7,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get install curl python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get update -y && sudo apt install docker-ce nodejs mongodb-clients -y
+apt-get update -y && sudo apt install docker.io nodejs mongodb-clients -y
 apt-get upgrade -y
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose

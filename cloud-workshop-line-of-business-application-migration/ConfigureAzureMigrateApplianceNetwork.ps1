17e7dbe9740f7d784b8a3d32b50aadb3f27e249e
Jonathan Tuliani <jtuliani@opsgility.com>
2019-05-17 11:07:00 +0100

    Removing...moved to public storage

--- cloud-workshop-line-of-business-application-migration/ConfigureAzureMigrateApplianceNetwork.ps1
+++
@@ -1,3 +0,0 @@
-# Connect Azure Migrate switch to the NAT network
-$adapter = Get-NetAdapter | ? { $_.Name -like "*Migrate*" }
-New-NetIPAddress -IPAddress 192.168.1.1 -PrefixLength 24 -InterfaceIndex $adapter.ifIndex

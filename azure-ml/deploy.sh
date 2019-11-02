3953519711b1616edca1fae330033b669da62683
Michael Washam <michael@opsgility.com>
2019-11-02 07:26:32 -0500

    adding ml script

---
+++ azure-ml/deploy.sh
@@ -0,0 +1,34 @@
+export DEBIAN_FRONTEND=noninteractive
+AZUREUSERNAME=$1
+AZUREPASSWORD=$2
+SUBID=$3
+LOCATION=$4
+RGNAME="azureml"
+WORKSPACE="AMLWorkspace"
+
+apt-get update -y
+apt-get upgrade -y
+
+apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | 
+    gpg --dearmor | 
+    tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | 
+    tee /etc/apt/sources.list.d/azure-cli.list
+
+apt-get update -y
+
+apt-get install azure-cli -y
+
+az extension add -n azure-cli-ml -y
+
+az login -u $AZUREUSERNAME -p $AZUREPASSWORD
+
+az group create -n $RGNAME -l $LOCATION
+
+az ml workspace create -w $WORKSPACE -g $rgname
+
+az ml computetarget create amlcompute -n cpu --min-nodes 1 --max-nodes 2 -s STANDARD_D2S_V3

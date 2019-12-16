b84eca9524b3d1e684449c942c2100e4e76f4a89
Michael Washam <michael@opsgility.com>
2019-12-16 13:35:17 -0600

    updates

--- oss-devops/labvmconfig.sh
+++ oss-devops/labvmconfig.sh
@@ -1,29 +1,29 @@
 export DEBIAN_FRONTEND=noninteractive
 #Install LXDE lxde.org and xrdp - (make sure to open 3389 on the NSG of the azure vm)
-apt-get update
-apt-get install -y lxde
-apt-get install -y xrdp
-/etc/init.d/xrdp start
+#apt-get update
+#apt-get install -y lxde
+#apt-get install -y xrdp
+#/etc/init.d/xrdp start
 
 #Prepare XWindows System
-sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
-sudo touch ~/.xsession
-echo "startlxde" > ~/.xsession
+#sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
+#sudo touch ~/.xsession
+#echo "startlxde" > ~/.xsession
 
 #installing visual studio code which can be launched from accessories
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
-apt-get update
-apt-get install -y code
+#curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+#mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+#sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+#sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
+#apt-get update
+#apt-get install -y code
 
 #install the Azure CLI using instructions from Azure.com
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
-apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-apt-get install -y apt-transport-https
-apt-get update && sudo apt-get install -y azure-cli
+#echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+#     sudo tee /etc/apt/sources.list.d/azure-cli.list
+#apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+#apt-get install -y apt-transport-https
+#apt-get update && sudo apt-get install -y azure-cli
 
 #install node.js and npm
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -

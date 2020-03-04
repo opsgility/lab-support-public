afae6c7b223005e4e2534cda66e120fca50b005c
Michael Washam <michael@opsgility.com>
2020-03-04 05:57:36 -0600

    adding tailored experience script

--- script-extensions/downloadfiles.ps1
+++ script-extensions/downloadfiles.ps1
@@ -23,3 +23,31 @@ if((Test-Path -Path $path) -eq $true)
 {
     Set-ItemProperty -Path $path -Name "TailoredExperiencesWithDiagnosticDataEnabled" -Value 0
 }
+
+
+$opsDir = "C:\SkillMeUp"
+
+if((Test-Path -Path $opsDir) -eq $false)
+{
+    New-Item -Path $opsDir -ItemType directory -Force
+}
+
+# Download scripts
+Write-Output "Download scripts"
+$downloads = @( `
+     "https://raw.githubusercontent.com/opsgility/lab-support-public/master/win10/no-personalized-experience.ps1" 
+    )
+
+$destinationFiles = @( `
+     "$opsDir\no-personalized-experience.ps1" `
+    )
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $downloads -Destination $destinationFiles
+
+# Register task to run post-reboot script once host is rebooted after Hyper-V install
+Write-Output "Register post-reboot script as scheduled task"
+$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $opsDir\no-personalized-experience.ps1"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+Register-ScheduledTask -TaskName "SetUpVMs" -Action $action -Trigger $trigger -Principal $principal

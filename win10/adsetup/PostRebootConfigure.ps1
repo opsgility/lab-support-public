1653becbd29ad95898850b555da50f3bbaeafb71
Kit Skinner <42873266+KitSkin@users.noreply.github.com>
2020-08-14 09:27:34 -0500

    Update to script to allow for retries

--- win10/adsetup/PostRebootConfigure.ps1
+++ win10/adsetup/PostRebootConfigure.ps1
@@ -20,13 +20,32 @@ $sourceFileUrl = "$repo/$ContentFile"
 $fileName = Split-Path -Path $sourceFileUrl -Leaf
 $destinationPath = Join-Path $Folder $fileName
 
-(New-Object Net.WebClient).DownloadFile($sourceFileUrl,$destinationPath);
+$maxTries=15
+$waitInSeconds=15
+$try=0
+Do {
+    try {
+        (New-Object Net.WebClient).DownloadFile($sourceFileUrl,$destinationPath);
+    } catch {
+        If (-not (Test-Path $destinationPath -ErrorAction SilentlyContinue)) {
+            Write-Error "Could not download $sourceFileUrl . Pausing for $waitInSeconds seconds before trying again."
+            Start-Sleep -Seconds $waitInSeconds
+        } else {
+            throw $Error[0]
+        }
+    }
 
-Write-Output "Extracting zip to $Folder..."
-(new-object -com shell.application).namespace($Folder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
+    $try++
+} Until ($try -ge $maxTries -or (Test-Path $destinationPath -ErrorAction SilentlyContinue))
 
-&"$Folder\DomainUpdate.ps1" -SharePath "$Folder\LabFiles"
+If (Test-Path $destinationPath -ErrorAction SilentlyContinue) {
+    Write-Output "Extracting zip to $Folder..."
+    (new-object -com shell.application).namespace($Folder).CopyHere((new-object -com shell.application).namespace($destinationPath).Items(),16)
 
+    &"$Folder\DomainUpdate.ps1" -SharePath "$Folder\LabFiles"
+} else {
+    Write-Error "Unable to download file.  Reboot to attempt to download again."
+}
 
 }
 

777a64787b15977c63e67f319978fd5087a4e89e
Kit Skinner <42873266+KitSkin@users.noreply.github.com>
2020-08-16 23:52:32 -0500

    Fixing username parameter

--- script-extensions/domain-join.ps1
+++ script-extensions/domain-join.ps1
@@ -1,5 +1,5 @@
-param($domain, $user, $password)
+param($domain, $user="demouser", $password)
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
-$user = "$domain\demouser"
+$user = "$domain\$user"
 $objCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $smPassword)
 Add-Computer -DomainName "$domain" -Credential $objCred -Restart -Force

d6eaadc5700a0b33f30bea104475caf733e5c2c3
Kit Skinner <42873266+KitSkin@users.noreply.github.com>
2020-08-17 00:06:21 -0500

    Reverting to previous version
    Putting back to previous version to ensure other issues do not creap up.

--- script-extensions/domain-join.ps1
+++ script-extensions/domain-join.ps1
@@ -1,5 +1,5 @@
-param($domain, $user="demouser", $password)
+param($domain, $user, $password)
 $smPassword = (ConvertTo-SecureString $password -AsPlainText -Force)
-$user = "$domain\$user"
+$user = "$domain\demouser"
 $objCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $smPassword)
 Add-Computer -DomainName "$domain" -Credential $objCred -Restart -Force

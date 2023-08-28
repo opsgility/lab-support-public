b31415ab797315fc1e6b717fa414764ae729e806
Garion Washam <gwasham@opsgility.com>
2023-08-28 12:28:51 -0500

    Updated Data Engineering script

--- live-hack-azure-data-engineering/configure-onprem-sql.ps1
+++ live-hack-azure-data-engineering/configure-onprem-sql.ps1
@@ -56,7 +56,7 @@ Set-ItemProperty -Path $HKCU -Name "CheckedUnattendLaunchSetting" -Value 0 -Type
 # Install Chrome
 $Path = $env:TEMP; 
 $Installer = "chrome_installer.exe"
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
+Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer
 Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait
 Remove-Item $Path\$Installer
 
